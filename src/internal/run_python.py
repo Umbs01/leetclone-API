118dd52cc1fd0225a8589c00d1b9b7f5099ed408
Umbs01 <overpowerpotato0@gmail.com>
2024-10-06 16:44:19 +0700

    minor changes

--- src/internal/run_python.py
+++ src/internal/run_python.py
@@ -12,7 +12,7 @@ def run_code(text):
         subprocess.run(['sudo', 'mv', 'script.py', f'{get_settings().SANDBOX_PATH}/box'], check=True)
 
         # Run the script with memory and time limits
-        result = subprocess.run(['sudo', 'isolate', '--box-id=0', '--mem=102400', '--time=3', '--run', '--', '/usr/bin/python3', 'script.py'],
+        result = subprocess.run(['sudo', 'isolate', '--box-id=0', '--mem=102400', '--time=3', '--run', '--', '/usr/bin/python3', f'{get_settings().SANDBOX_PATH}/box/script.py'],
                                 check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         # Capture output and errors

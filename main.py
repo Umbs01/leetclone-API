b66811722302aa9c79b6831801647d29b6103def
Umbs01 <overpowerpotato0@gmail.com>
2024-11-17 13:55:39 +0700

    minor changes

--- main.py
+++ main.py
@@ -2,6 +2,4 @@ import uvicorn
 import os
 
 if __name__ == '__main__':
-  if not os.path.exists("images"):
-    os.makedirs("images")
   uvicorn.run("src.app:app", host="0.0.0.0", port=4000, reload=True)

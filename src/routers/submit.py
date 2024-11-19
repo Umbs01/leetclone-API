71e2135252b903f799f95bb3e0e7e9d482159b7a
Umbs01 <overpowerpotato0@gmail.com>
2024-11-19 18:43:11 +0700

    added security measures for submitting a problem

--- src/routers/submit.py
+++ src/routers/submit.py
@@ -4,12 +4,15 @@ from ..utils.dependencies import get_db
 from ..models.submission import SubmissionModel
 from ..internal.submissions_crud import submit, get_submissions_by_user
 from ..internal.users_crud import get_user_by_student_id
-
+from ..utils.dependencies import get_current_user 
 
 router = APIRouter(prefix="/submit", tags=["submit"], responses={404: {"description": "Not found"}})
 
 @router.post("")
-def submitproblem(submission: SubmissionModel, db = Depends(get_db)):
+def submitproblem(submission: SubmissionModel, token: str, db = Depends(get_db)):
+    user = get_current_user(db, token)
+    if user.student_id != submission.owner: #type: ignore
+        raise HTTPException(status_code=400, detail="Student id does not match")
     try:
         get_user_by_student_id(db, submission.owner)
     except:
